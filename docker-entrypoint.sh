1c79176fd0e808b536051c3d78058be1cc6efdcd
Raymond Mouthaan <raymondmmouthaan@gmail.com>
2018-03-13 14:35:07 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,3 @@
-###
-# credits: stefanprodan/swarmprom
-###
 #!/bin/sh -e
 
 NODE_NAME=$(cat /etc/nodename)
@@ -9,19 +6,3 @@ echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\
 set -- /bin/node_exporter "$@"
 
 exec "$@"
-
-# #!/bin/sh -e
-
-# if [ -z ${HOST_HOSTNAME+x} ]; then
-#   echo "Environment variable 'HOST_HOSTNAME' not set, we won't add the hostname metric."
-# else
-#   host_hostname=$(cat ${HOST_HOSTNAME})
-#   echo "host{host=\"$host_hostname\", node=\"$(hostname)\"} 1" > /etc/node-exporter/host_hostname.prom
-# fi
-
-# # if command starts with an option, prepend node-exporter binary
-# if [ "${1:0:1}" = '-' ]; then
-#   set -- $NODE_EXPORTER_BIN "$@"
-# fi
-
-# exec "$@"
